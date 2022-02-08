e711148db53937fa259101e15bb432642369ebc8
Victor Gordian <victorgordian103@gmail.com>
2022-02-07 22:22:17 -0800

    changed everything to be just on App

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,12 +1,68 @@
 import React from 'react'
-import Form from './Form'
-import Todo from './Todo'
+
+
+const initialState ={
+  tasks: [{task: 'Move the car', id:2343 , completed: false}]
+}
+
+const URL = 'http://localhost:9000/api/todos'
+
+
 export default class App extends React.Component {
+  state = initialState
+
+  componentDidMount(){
+    this.state.tasks
+  }
+
+  onChange = e => {
+    const { value, id } = e.target
+    this.setState({ ...this.state, [id]: value })
+  }
+  
+
+  onSubmit = event => {
+    event.preventDefault()
+    const newTodo = {
+      id: Date.now(),
+      task: this.state.taskInput,
+      completed: false
+
+    }
+    this.setState({ ...this.state, tasks: [ ...this.state.tasks, newTodo], taskInput: ''})
+  }
+
+  onClick = (id) => {
+    this.setState({ ...this.state,
+      tasks: this.state.tasks.map(task => {
+        if (task.id === id) {
+          return {...task, completed: !task.completed}
+        } else {
+          return task
+        }
+      })
+    })
+  }
+
+ 
+  
   render() {
     return (
       <div>
-        <Form />
-        <Todo />
+        <div>
+         <ul>
+           {
+             this.state.tasks.map(task => (
+              <li key={task.id} onClick={() => this.onClick(task.id)}> {task.task}</li>
+              ))
+           }
+         </ul>
+        </div>
+        <form onSubmit={this.onSubmit}>
+          <input onChange={this.onChange} value={this.state.taskInput} type="text" id="taskInput" placeholder="add Task"/>
+          <input type="submit" />
+        
+        </form>
       </div>
     )
   }
